9b6c2232cac060fffcb77b852e3f13ee9806a82c
Agata <frealka@gmail.com>
2019-03-30 22:49:21 +0100

    date picker implemented

--- app/src/main/java/com/example/agata/todolist/AddItemActivity.kt
+++ app/src/main/java/com/example/agata/todolist/AddItemActivity.kt
@@ -1,16 +1,41 @@
 package com.example.agata.todolist
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_item.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddItemActivity : AppCompatActivity() {
+
+    private val cal : Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+
+        deadlineEditText!!.setOnClickListener {
+            DatePickerDialog(this@AddItemActivity,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        deadlineEditText!!.text = sdf.format(cal.getTime())
     }
 
 }
