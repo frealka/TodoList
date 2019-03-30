b5858987bf1b9a3373ae4778d7be97171542a6a5
Agata <frealka@gmail.com>
2019-03-30 21:48:15 +0100

    add item layout created

---
+++ app/src/main/java/com/example/agata/todolist/AddItemActivity.kt
@@ -0,0 +1,16 @@
+package com.example.agata.todolist
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_add_item.*
+
+class AddItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+
+}
--- app/src/main/java/com/example/agata/todolist/MainActivity.kt
+++ app/src/main/java/com/example/agata/todolist/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.agata.todolist
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun addItem(view: View){
+        val newIntent = Intent(this, AddItemActivity::class.java)
+        startActivityForResult(newIntent, addItemRequestCode)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if((requestCode == addItemRequestCode) and (resultCode == Activity.RESULT_OK)){
+            Log.d(TAG, "otrzymałem informację o kodzie $addItemRequestCode")
+        }
+    }
+
+    companion object {
+        const val addItemRequestCode: Int = 134
+        const val TAG: String = "MainActivity"
     }
 }
